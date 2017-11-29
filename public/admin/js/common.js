250955cee2631d46ef68f399863ba8dd345f723e
wangmengru1995 <1510189536@qq.com>
2017-11-29 21:44:29 +0800

    首页功能完成[F

---
+++ public/admin/js/common.js
@@ -0,0 +1,38 @@
+$(function () {
+
+    //侧边栏二级菜单显示和隐藏
+    //因为考虑到以后可能会增加内容，所以不能写死
+    $(".child").prev().click(function () {
+        $(this).next().slideToggle();
+        $(this).toggleClass("now");
+    })
+
+
+    //点击隐藏侧边栏功能
+    $('.icon-menu').click(function () {
+        $(".lt-aside").toggleClass("now");
+        $(".lt-main").toggleClass("now");
+    })
+
+
+    //点击退出当前用户
+    $(".icon-logout").click(function () {
+
+        $("#logoutModal").modal('show');
+
+        //在里面给btn-logout注册点击事件，如果多次取消退出，会导致该事件被多次注册，需要在点击前解绑点击事件
+        $(".btn-logout").off().click(function () {
+            $.get("/employee/employeeLogout", function (data) {
+                if (data.success) {
+                    location.href = "login.html";
+                }
+            })
+        })
+    })
+
+
+
+
+
+
+})
