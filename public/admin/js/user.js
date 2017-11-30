18d15312db46a0bc92f0125a851ae1f0ecbc41fe
wangmengru1995 <1510189536@qq.com>
2017-11-30 14:28:34 +0800

    用户管理模块完成

--- public/admin/js/user.js
+++ public/admin/js/user.js
@@ -1,3 +1,64 @@
-$(function(){
-  
+$(function () {
+  //准备模板数据
+  var currentPage = 1;
+  var pageSize = 5;
+
+  render();
+
+  //用户操作功能
+  $("tbody").on("click", ".btn", function () {
+    //显示模态框
+    $("#userModal").modal("show");
+
+    var id = $(this).parent().data("id");
+    var isDelete = $(this).hasClass("btn-danger") ? "0" : "1";
+    // console.log(isDelete);
+
+    $(".btn-confirm").off().on("click", function () {
+      //模态框隐藏
+      $("#userModal").modal("hide");
+      //发送ajax
+      $.ajax({
+        type: "post",
+        url: "/user/updateUser",
+        data: {
+          id: id,
+          isDelete: isDelete
+        },
+        success: function (data) {
+          if (data.success) {
+            render();
+          }
+        }
+      })
+    })
+  })
+
+
+  function render() {
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      success: function (data) {
+        // console.log(data);
+        $("tbody").html(template("userTpl", data));
+
+        //分页功能
+        $("#userPaginator").bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          numberOfPages: 5,
+          totalPages: Math.ceil(data.total / data.size),
+          //onPageClicked 有四个参数，要按顺序写参数
+          onPageClicked: function (a, b, c, page) {
+            currentPage = page;
+            render();
+          }
+        })
+      }
+    })
+  }
 })
