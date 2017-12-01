f172ea5e85aa3f0f7a86fb489da58610338b3a14
wangmengru1995 <1510189536@qq.com>
2017-12-01 17:08:43 +0800

    侧边栏功能完成

--- public/project/js/common.js
+++ public/project/js/common.js
@@ -10,5 +10,31 @@ $(function(){
     })
 
 
+    //侧边栏二级菜单显示隐藏
+    $(".child").prev().on("click",function(){
+      $(this).next().slideToggle();
+    })
 
+    //点击隐藏侧边栏
+    $(".icons_menu").click(function(){
+      $(".lt-aside").toggleClass("now");
+      $(".lt-main").toggleClass("now");
+    })
+
+    //点击显示退出的模态框
+    $(".icons_logout").click(function(){
+      $("#logoutModal").modal("show");
+    })
+
+    $(".btn_logout").click(function(){
+      $.ajax({
+        type: "get",
+        url: "/employee/employeeLogout",
+        success: function(data){
+          if(data.success){
+            location.href = "login.html";
+          }
+        }
+      })
+    })
 })
